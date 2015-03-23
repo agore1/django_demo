e6705353ea23a89d9c878fd449ca91bc06a6b3ba
Austin Gore <austin.gore@gmail.com>
2015-03-23 10:47:17 -0400

    Added django files.

---
+++ polls/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from polls.models import Question
+
+# Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets= [
+		(None, {'fields': ['question_text']}),
+		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+	]
+
+	#fields = ['pub_date', 'question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
